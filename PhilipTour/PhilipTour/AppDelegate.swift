d1ebf38f6369c196240030cbd0d7565602328647
정필립/게임플랫폼클라팀/NE <philip.chung@nhn.com>
2021-01-08 01:15:42 +0900

    Add launching screen

--- PhilipTour/PhilipTour/AppDelegate.swift
+++ PhilipTour/PhilipTour/AppDelegate.swift
@@ -13,7 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // 3초 뒤 화면 전환
+        Thread.sleep(forTimeInterval: 3)
         return true
     }
 
