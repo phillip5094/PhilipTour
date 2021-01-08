2dd590dddba7113c9a953c78429faafae7377b9f
정필립/게임플랫폼클라팀/NE <philip.chung@nhn.com>
2021-01-08 21:52:49 +0900

    Add launch screen elements

--- PhilipTour/PhilipTour/LoginViewController.swift
+++ PhilipTour/PhilipTour/LoginViewController.swift
@@ -9,13 +9,9 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var navigationBar: UINavigationBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 네비게이션바 bottom line 지우기
-        self.navigationBar.setValue(true, forKey: "hidesShadow")
 
     }
 
